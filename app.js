0a64ac1efd382faa4fa76c33870bcd99a379f9c9
Mauri García <63955780+maurigarciavg@users.noreply.github.com>
2026-01-21 20:44:39 +0000

    ejercicio terminado

---
+++ app.js
@@ -0,0 +1,39 @@
+const sum = (a,b) => {
+    return a + b
+};
+
+
+
+
+let oneEuroIs = {
+    "JPY": 156.5,
+    "USD": 1.07,
+    "GBP": 0.87,
+}
+
+// "$" "€" "¥" "£"
+
+
+
+function fromEuroToDollar (euros) {
+    let dollar = euros * oneEuroIs.USD;
+    return dollar
+}
+
+function fromDollarToYen (dollar) {
+    let euros = dollar / oneEuroIs.USD;
+    let yen = euros * oneEuroIs.JPY;
+    return yen;
+}
+function fromYenToPound (yen) {
+    let euros = yen / oneEuroIs.JPY;
+    let pounds = euros * oneEuroIs.GBP;
+    return Number(pounds.toFixed(4));
+}
+
+
+console.log(fromYenToPound(1));
+
+
+
+module.exports = { sum, oneEuroIs, fromEuroToDollar, fromDollarToYen, fromYenToPound};
---
+++ test.js
@@ -0,0 +1,20 @@
+const { sum, fromEuroToDollar, oneEuroIs, fromDollarToYen, fromYenToPound } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum (14, 9);
+    expect(total).toBe(23);
+});
+
+test('calculate the divisa change between euros 1 and dollars 1.07 ', () => {
+    let dollars = fromEuroToDollar(1);
+    expect(dollars).toBe(oneEuroIs.USD);
+});
+
+test('calculate the divisa change between dollar to yen ', () => {
+    let yen = fromDollarToYen(1);
+    expect(yen).toBeCloseTo(146.26)
+});
+test('calculate the divisa change between yen to pound ', () => {
+    let pound = fromYenToPound(1);
+    expect(pound).toBeCloseTo(0.0056)
+});
